d46cff41e8136cecb2cc5f4fbd25c5de7b77e521
simizzz <merander207203@gmail.com>
2024-05-19 17:46:53 +0500

    add prometheus and grafana apps into container

--- src/app.py
+++ src/app.py
@@ -7,16 +7,20 @@ from api.admin import router as admin_router
 from api.media import router as media_router
 from api.all_roles import router as all_roles_router
 from fastapi.middleware.cors import CORSMiddleware
+from prometheus_fastapi_instrumentator import Instrumentator
 
 sessionmanager.init(settings.DB_CONFIG)
 
 app = FastAPI(title="Restaurant Table Reservation Server")
+
 
 app.include_router(client_router, prefix="/api")
 app.include_router(superadmin_router, prefix="/api")
 app.include_router(admin_router, prefix="/api")
 app.include_router(media_router, prefix="/api")
 app.include_router(all_roles_router, prefix="/api")
+
+
 
 origins = [
     "*"
@@ -30,3 +34,4 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+Instrumentator().instrument(app).expose(app)
