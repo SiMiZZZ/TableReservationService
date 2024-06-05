bd524e135dd21e284049ab007a8c1bbb73283dec
simizzz <merander207203@gmail.com>
2024-06-06 01:36:10 +0500

    HOTFIX: remove unused import

--- src/app.py
+++ src/app.py
@@ -4,7 +4,6 @@ from services.database import sessionmanager
 from api.client import router as client_router
 from api.superadmin import router as superadmin_router
 from api.admin import router as admin_router
-from api.media import router as media_router
 from api.all_roles import router as all_roles_router
 from fastapi.middleware.cors import CORSMiddleware
 from prometheus_fastapi_instrumentator import Instrumentator
@@ -29,7 +28,6 @@ app = FastAPI(title="Restaurant Table Reservation Server")
 app.include_router(client_router, prefix="/api")
 app.include_router(superadmin_router, prefix="/api")
 app.include_router(admin_router, prefix="/api")
-app.include_router(media_router, prefix="/api")
 app.include_router(all_roles_router, prefix="/api")
 
 
